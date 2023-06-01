da9143435f7406088262f41fdaa8fda0683bf1a0
dan-vds <danielvds101@gmail.com>
2023-06-01 17:52:11 +1200

    Added Stack implementation as I wrote it on coderunner

---
+++ src/main/java/nz/ac/auckland/se281/datastructures/Stack.java
@@ -0,0 +1,34 @@
+package nz.ac.auckland.se281.datastructures;
+
+public class Stack<T> {
+  private LinkedList<T> stack;
+
+  public Stack() {
+    stack = new LinkedList<T>();
+  }
+
+  public void push(T data) {
+    stack.addFirst(data);
+  }
+
+  public T pop() {
+    return stack.removeFirst();
+  }
+
+  public T peek() {
+    return stack.getFirst();
+  }
+
+  public int size() {
+    return stack.size();
+  }
+
+  public boolean isEmpty() {
+    return stack.isEmpty();
+  }
+
+  @Override
+  public String toString() {
+    return stack.toString();
+  }
+}

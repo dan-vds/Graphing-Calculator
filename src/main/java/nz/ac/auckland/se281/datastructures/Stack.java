e56db0638db3cac1ec970e62ad8e9c480a63a43d
dan-vds <danielvds101@gmail.com>
2023-06-05 13:29:22 +1200

    added javadoc to Stack

--- src/main/java/nz/ac/auckland/se281/datastructures/Stack.java
+++ src/main/java/nz/ac/auckland/se281/datastructures/Stack.java
@@ -3,26 +3,52 @@ package nz.ac.auckland.se281.datastructures;
 public class Stack<T> {
   private LinkedList<T> stack;
 
+  /** Constructor for Stack, initialises stack as a new LinkedList */
   public Stack() {
     stack = new LinkedList<T>();
   }
 
+  /**
+   * Adds a new node to the top of the stack
+   *
+   * @param data the data to be stored in the new node
+   */
   public void push(T data) {
     stack.addFirst(data);
   }
 
+  /**
+   * Removes the top node from the stack
+   *
+   * @return the data stored in the node being removed
+   */
   public T pop() {
     return stack.removeFirst();
   }
 
+  /**
+   * Returns the top node in the stack
+   *
+   * @return the data stored in the top node
+   */
   public T peek() {
     return stack.getFirst();
   }
 
+  /**
+   * Computes the size of the stack
+   *
+   * @return the size of the stack
+   */
   public int size() {
     return stack.size();
   }
 
+  /**
+   * Checks if the stack is empty
+   *
+   * @return true if the stack is empty, false otherwise
+   */
   public boolean isEmpty() {
     return stack.isEmpty();
   }

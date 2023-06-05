104c87179bd4fc41ebca36ef3d1f45c79e1e7748
dan-vds <danielvds101@gmail.com>
2023-06-05 15:15:43 +1200

    Added javadoc and comments to Node

--- src/main/java/nz/ac/auckland/se281/datastructures/Node.java
+++ src/main/java/nz/ac/auckland/se281/datastructures/Node.java
@@ -2,41 +2,90 @@ package nz.ac.auckland.se281.datastructures;
 
 public class Node<T> {
 
+  // declaring instance variables for data stored, next and previous nodes
   private T data;
   private Node<T> next;
   private Node<T> prev;
 
+  /**
+   * Constructor for Node, initialises data and next as null.
+   *
+   * @param data the data to be stored in the node.
+   */
   public Node(T data) {
+    // initialise data and next as null
     this.data = data;
     next = null;
   }
 
+  /**
+   * Gets the data stored in the node.
+   *
+   * @return the data stored in the node.
+   */
   public T getData() {
+    // return the data stored in the node
     return data;
   }
 
+  /**
+   * Sets the data stored in the node.
+   *
+   * @param data the data to be stored in the node.
+   */
   public void setData(T data) {
+    // set the data stored in the node
     this.data = data;
   }
 
+  /**
+   * Gets the next node.
+   *
+   * @return the next node.
+   */
   public Node<T> getNext() {
+    // return the next node
     return next;
   }
 
+  /**
+   * Sets the next node.
+   *
+   * @param next the next node.
+   */
   public void setNext(Node<T> next) {
+    // set the next node
     this.next = next;
   }
 
+  /**
+   * Gets the previous node.
+   *
+   * @return the previous node.
+   */
   public Node<T> getPrev() {
+    // return the previous node
     return prev;
   }
 
+  /**
+   * Sets the previous node.
+   *
+   * @param prev the previous node.
+   */
   public void setPrev(Node<T> prev) {
+    // set the previous node
     this.prev = prev;
   }
 
+  /**
+   * Overrides the string representation of the node.
+   *
+   * @return the string representation of the node.
+   */
   @Override
   public String toString() {
+    // return the string representation of the node
     return data.toString();
   }
 }

b75598b7c29e8016333a5040a54252eb1c0c26c7
dan-vds <danielvds101@gmail.com>
2023-06-05 15:12:56 +1200

    added comments to stack and queue

--- src/main/java/nz/ac/auckland/se281/datastructures/Queue.java
+++ src/main/java/nz/ac/auckland/se281/datastructures/Queue.java
@@ -6,45 +6,55 @@ public class Queue<T> {
 
   /** Constructor for Queue, initialises queue as a new LinkedList */
   public Queue() {
+    // initialise queue as a new LinkedList
     queue = new LinkedList<T>();
   }
 
   /**
-   * Adds a new node to the end of the queue
+   * Adds a new node to the end of the queue.
    *
-   * @param data the data to be stored in the new node
+   * @param data the data to be stored in the new node.
    */
   public void enqueue(T data) {
+    // add the new node to the end of the queue
     queue.add(data);
   }
 
   /**
-   * Removes the first node from the queue
+   * Removes the first node from the queue.
    *
-   * @return the data stored in the node being removed
+   * @return the data stored in the node being removed.
    */
   public T dequeue() {
+    // remove the first node from the queue
     return queue.removeFirst();
   }
 
   /**
-   * Returns the first node in the queue
+   * Returns the first node in the queue.
    *
-   * @return the data stored in the first node
+   * @return the data stored in the first node.
    */
   public T peek() {
+    // return the first node in the queue
     return queue.getFirst();
   }
 
   /**
-   * Computes the size of the queue
+   * Computes whether the queue is empty or not.
    *
-   * @return the size of the queue
+   * @return true if the queue is empty, false otherwise.
    */
   public boolean isEmpty() {
+    // return
     return queue.isEmpty();
   }
 
+  /**
+   * Overrides the string representation of the queue.
+   *
+   * @return the string representation of the queue.
+   */
   @Override
   public String toString() {
     return queue.toString();
--- src/main/java/nz/ac/auckland/se281/datastructures/Stack.java
+++ src/main/java/nz/ac/auckland/se281/datastructures/Stack.java
@@ -3,58 +3,65 @@ package nz.ac.auckland.se281.datastructures;
 public class Stack<T> {
   private LinkedList<T> stack;
 
-  /** Constructor for Stack, initialises stack as a new LinkedList */
+  /** Constructor for Stack, initialises stack as a new LinkedList. */
   public Stack() {
+    // initialise stack as a new LinkedList
     stack = new LinkedList<T>();
   }
 
   /**
-   * Adds a new node to the top of the stack
+   * Adds a new node to the top of the stack.
    *
-   * @param data the data to be stored in the new node
+   * @param data the data to be stored in the new node.
    */
   public void push(T data) {
+    // add the new node to the top of the stack
     stack.addFirst(data);
   }
 
   /**
-   * Removes the top node from the stack
+   * Removes the top node from the stack.
    *
-   * @return the data stored in the node being removed
+   * @return the data stored in the node being removed.
    */
   public T pop() {
+    // remove the top node from the stack
     return stack.removeFirst();
   }
 
   /**
-   * Returns the top node in the stack
+   * Returns the top node in the stack.
    *
-   * @return the data stored in the top node
+   * @return the data stored in the top node.
    */
   public T peek() {
+    // return the top node in the stack
     return stack.getFirst();
   }
 
   /**
-   * Computes the size of the stack
+   * Computes the size of the stack.
    *
-   * @return the size of the stack
+   * @return the size of the stack.
    */
   public int size() {
+    // return the size of the stack
     return stack.size();
   }
 
   /**
-   * Checks if the stack is empty
+   * Checks if the stack is empty.
    *
-   * @return true if the stack is empty, false otherwise
+   * @return true if the stack is empty, false otherwise.
    */
   public boolean isEmpty() {
+    // return true if the stack is empty, false otherwise
     return stack.isEmpty();
   }
 
   @Override
   public String toString() {
+    // return the string representation of the stack
     return stack.toString();
   }
 }
